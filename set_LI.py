c0f8f910f950f9b168ae61aed7e0ee793111f7ca
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2019-12-08 20:35:13 +0100

    Update set_LI.py

--- set_LI.py
+++ set_LI.py
@@ -3,17 +3,21 @@ def set_LI():
 
     from DayPhase import DayPhase
     from recv_light import recv_light
+    from datetime import datetime
 
+    current_time = datetime.now()
+    current_hour = current_time.hour
+    
     light_intensity = recv_light()
     day_phase = DayPhase()
 
-    if light_intensity <= day_phase.MORNING:
-        return 1
-    elif light_intensity <= day_phase.MIDDAY:
-        return 2
-    elif light_intensity <= day_phase.AFTERNOON:
-        return 3
-    elif light_intensity <= day_phase.EVENING:
-        return 4
+    if light_intensity <= day_phase.MORNING and current_hour <= 8:
+        return "MORNING"
+    elif light_intensity <= day_phase.MIDDAY and current_hour <= 13:
+        return "MIDDAY"
+    elif light_intensity <= day_phase.AFTERNOON and current_hour <= 19:
+        return "ATERNOON"
+    elif light_intensity <= day_phase.EVENING and current_hour <= 22:
+        return "EVENING"
     else:
-        return 5
+        return "NIGHT"
